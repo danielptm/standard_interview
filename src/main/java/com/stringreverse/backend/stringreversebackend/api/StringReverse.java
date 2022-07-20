b5e2b2e59cfbeb7497cdcab7b8aafd074ba44c35
Daniel Tuttle <daniel.tuttle@standard.com>
2022-07-20 10:39:52 -0700

    made some small changes

--- src/main/java/com/stringreverse/backend/stringreversebackend/api/StringReverse.java
+++ src/main/java/com/stringreverse/backend/stringreversebackend/api/StringReverse.java
@@ -1,11 +1,8 @@
 package com.stringreverse.backend.stringreversebackend.api;
 
-import com.stringreverse.backend.stringreversebackend.model.Payload;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
